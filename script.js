b2cf88b300a338a402062a26432b847be8c84008
kinza <kinzaij@gmail.com>
2025-06-17 22:35:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,285 @@
+const socket = io("http://localhost:3000");
+
+const user = {}
+socket.on("connect", () => {
+  console.log("✅ Connected to server!", socket.id);
+});
+
+socket.on("disconnect", () => {
+  console.log("❌ Disconnected from server");
+});
+    
+socket.on("number-received", val => selectNumber(val))
+
+function selectNumber(number){   
+  for(let i=1; i<=25; i++){
+    document.getElementById(`my-cell-${i}`).disabled = false;
+    if(document.getElementById(`my-cell-${i}`).innerText === number)
+      document.getElementById(`my-cell-${i}`).classList.add('selected')
+    }
+  }
+
+function registerUser(event){
+    event.preventDefault()
+    user.name = document.getElementById("user").value;
+    if(socket.connected)
+      socket.emit("register", user.name);
+    document.getElementById("user-form").classList.add("d-none")
+    document.getElementById("loader").classList.remove("d-none")
+}
+
+socket.on("yourID", (id) => {
+  document.getElementById("myID").innerText = id;
+  user.id = id;
+  document.getElementById("connect-form").classList.remove("d-none")
+  document.getElementById("loader").classList.add("d-none")
+})
+
+
+function connectUser(event){
+  event.preventDefault();
+  let pId = document.getElementById("id").value;
+  document.getElementById("connect-form").classList.add("d-none")
+  document.getElementById("loader").classList.remove("d-none")
+  socket.emit("askConnection", user.id, pId);
+}
+
+socket.on("connectionRequest", (pid, pname) => {
+  document.getElementById("loader").classList.add("d-none");
+  document.getElementById("pName").innerText = pname;
+  document.getElementById("pName").setAttribute("data-info", pid);
+  document.getElementById("permission-form").classList.remove("d-none");
+});
+
+
+function connectPartner(event, res){
+  event.preventDefault();
+  document.getElementById("permission-form").classList.add("d-none");
+  document.getElementById("loader").classList.remove("d-none");
+  let pId = document.getElementById("pName").getAttribute("data-info")
+  if(res === "yes")
+    socket.emit("connectWith", pId)
+}
+
+socket.on("room-connected", (room, uName, pName) => {
+  user.room = room
+  document.getElementById("loader").classList.add("d-none");
+  document.getElementById("connect-form").classList.add("d-none");
+  console.log("my Name:", uName, "yourName:", pName)
+})
+
+let gameOver = false;
+// Generate Numbers in random order
+function generateNumbers(e){
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  for(let i=numbers.length-1; i>0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  for(let i=1; i<=25; i++){
+    document.getElementById(`${e}-cell-${i}`).innerText = numbers[i-1]
+  }
+}
+// render again
+function renderGame(e){
+  generateNumbers(e);
+  for(let i=1; i<=25; i++){
+    if(document.getElementById(`${e}-cell-${i}`).classList.contains('selected')){
+      document.getElementById(`${e}-cell-${i}`).classList.remove('selected')
+    }
+  }
+    for(let i=0; i < 5; i++){
+      if(document.querySelectorAll(`#${e}Bingo span`)[i].classList.contains('set'))
+      document.querySelectorAll(`#${e}Bingo span`)[i].classList.remove('set')
+    }
+}
+
+// Calculating Score
+function countScore(e){
+  let score = 0
+  if(document.getElementById(`${e}-cell-1`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-2`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-3`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-4`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-5`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-6`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-7`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-8`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-9`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-10`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-11`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-12`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-13`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-14`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-15`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-16`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-17`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-18`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-19`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-20`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-21`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-22`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-23`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-24`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-25`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  
+  if(document.getElementById(`${e}-cell-1`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-6`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-11`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-16`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-21`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-2`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-7`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-12`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-17`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-22`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-3`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-8`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-13`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-18`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-23`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-4`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-9`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-14`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-19`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-24`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-5`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-10`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-15`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-20`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-25`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-1`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-7`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-13`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-19`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-25`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  if(document.getElementById(`${e}-cell-5`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-9`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-13`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-17`).classList.contains('selected') 
+  && document.getElementById(`${e}-cell-21`).classList.contains('selected') 
+  ){
+    if(score < 5)
+      score++;
+  }
+  return score;
+}
+
+function finishGame(e){
+  if (gameOver) return; // prevent multiple triggers
+    gameOver = true;
+  document.querySelector('.result').classList.remove('d-none');
+  if(e =='my'){
+    document.querySelector('.result #result-data').innerText = "WON...👏";
+    socket.emit("finished", user.room)
+  }
+  else{
+    document.querySelector('.result #result-data').innerText = "OOPS, YOU LOSE...👎";
+  }
+}
+
+function gameRefresh(){
+  gameOver = false;
+  document.querySelector('.result').classList.add('d-none');
+  document.getElementById('numberOrder').disabled = false;
+  renderGame('my');
+}
+
+function scoring(e){
+  let s = 'my';
+
+  // count score
+  let score = countScore(`${e}`);
+  // mark bingo characters
+  if(score > 0 && !gameOver){
+    for(let i=0; i < score; i++){
+      document.querySelectorAll(`#${e}Bingo span`)[i].classList.add('set')
+    }
+  }
+  
+  // if won
+  if(score === 5){
+    finishGame(e);
+  }
+}
+// when a number is clicked
+function cellClick(e){
+  document.getElementById('numberOrder').disabled = true;
+
+  const val = e.target.innerText;
+
+  if(e.target.classList.contains('selected')){
+    alert('already selected')
+    return;
+  }
+  // mark selected number
+  e.target.classList.add('selected');
+  
+  scoring('my');
+  
+  for(let i = 1; i<= 25; i++)
+    document.getElementById(`my-cell-${i}`).disabled = true;
+
+
+  if (socket && socket.connected){
+  socket.emit("number-selected", user.room, val)
+  }
+}
+window.addEventListener('load', function() {
+  document.getElementById("loader").classList.add("d-none");
+  generateNumbers('my');
+
+  setTimeout(function() {
+    const element = document.querySelector('.welcome');
+    element.classList.add('exit');
+    element.addEventListener('transitionend', () => {
+      element.remove();
+    });
+    document.querySelector("main").classList.remove("d-none")
+      }, 1500) 
+});
+
---
+++ server.js
@@ -0,0 +1,67 @@
+const PORT = '3000';
+const io = require("socket.io")(PORT, {
+  cors: {
+    origin:['http://localhost:5500']
+  }
+})
+
+const users = [] // <- This is scoped inside `socket.on("connection")`
+
+
+io.on("connection", socket => {
+  socket.on("number-selected", (room, val) => {
+    io.to(room).emit("number-received", val )
+  })
+
+
+  socket.on("finished", (room) => {
+      io.to(room).emit("gameEnd")
+  })
+
+
+  socket.on("register", (name) => {
+    let user = {
+      name: name,
+      id: socket.id,
+      partner: null,
+      partnerId: null
+    }
+
+    users.push(user);
+    socket.emit("yourID", user.id)
+  })
+
+  socket.on("askConnection", (uID, pId) => {
+    console.log(uID, pId)
+    // let tempPartner = users.find(u => u.id === pId);
+    let tempuser = users.find(u => u.id === uID);
+    let tempPartnerSocket = io.sockets.sockets.get(pId);
+      if(tempuser && tempPartnerSocket)
+        tempPartnerSocket.emit("connectionRequest", tempuser.id, tempuser.name);
+      else
+        socket.emit("error", "partner not found");
+  })
+
+  socket.on("connectWith", (pId) => {
+    const me = users.find(u => u.id === socket.id);
+    const partner = users.find(u => u.id === pId);
+
+    if(me && partner){
+      me.partner = partner.name;
+      me.partnerId = partner.id;
+
+      partner.partner = me.name
+      partner.partnerId = me.id;
+
+      const roomName = `room-${me.id}-${partner.id}`;
+      socket.join(roomName);
+      let partnerSocket = io.sockets.sockets.get(pId);
+      if(partnerSocket)
+        partnerSocket.join(roomName);
+        io.to(roomName).emit("room-connected", roomName, me.name, partner.name)
+      }else
+      tempPartnerSocket.emit("error", "partner not found");
+    })
+
+})
+
